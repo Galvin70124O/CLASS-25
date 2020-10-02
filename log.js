b01bb4abe95b532fe62541b7bfad0ab7464c59d5
Galvin70124O <71276521+Galvin70124O@users.noreply.github.com>
2020-10-02 20:58:13 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,14 @@
+class log extends Parent{
+    constructor(x, y,height,angle) {
+      super(x,y,20,height)
+      Matter.Body.setAngle (this.body,angle)
+      this.image=loadImage("sprites/wood2.png")
+      
+     
+      
+      
+    }
+    
+    
+  };
+  
